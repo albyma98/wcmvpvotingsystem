55c49a13e674dbc37f65108f6482d52b6006f0a2
albyma98 <marradevelop@gmail.com>
2025-10-25 01:35:54 +0200

    tryingv2

--- wcmvpvs-front/src/components/QRScanner.vue
+++ wcmvpvs-front/src/components/QRScanner.vue
@@ -346,26 +346,3 @@ defineExpose({
 </style>
 
 
-<!--<template>
-  <qrcode-stream @decode="onDecode" @init="onInit" />
-</template>
-
-<script setup>
-import { ref } from 'vue';
-
-const lastResult = ref('');
-
-const onDecode = result => {
-  lastResult.value = result;
-  // gestisci il risultato del QR qui
-};
-
-const onInit = async (promise) => {
-  try {
-    await promise;
-    // fotocamera disponibile
-  } catch (error) {
-    console.error('Impossibile inizializzare il lettore QR:', error);
-  }
-};
-</script>-->
--- wcmvpvs-front/src/main.js
+++ wcmvpvs-front/src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
-import VueQrcodeReader from 'vue-qrcode-reader';
+import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from 'vue-qrcode-reader'
 
-const app = createApp(App);
-app.use(VueQrcodeReader);
+const app = createApp(App)
+app.component('QrcodeStream', QrcodeStream)
+app.component('QrcodeDropZone', QrcodeDropZone)
+app.component('QrcodeCapture', QrcodeCapture)
 app.mount('#app');
